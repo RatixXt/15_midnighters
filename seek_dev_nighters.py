76ce5937d86f4697b41b0f33a12733e11f60921e
Shcherbakov Alexey <lovelessxt@gmail.com>
2016-10-30 22:03:20 +0300

    Проведен рефакторинг кода
    Изменена структура данных возвращаемая функцией load_attempts, теперь это не список, а словарь.
    
    Изменен способ сравнения времени для нахождения полуночников. Теперь нет преобразования к int, время сравнивается со временем 5:00:00.

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import requests
-from datetime import datetime
+import datetime
 from pytz import timezone
 
 DEVMAN_API_URL = 'https://devman.org/api/challenges/solution_attempts/'
-INT_MORNING_TIME = 500
+MORNING_TIME = datetime.time(hour=5, minute=0, second=0, microsecond=0, tzinfo=None)
 FIRST_PAGE = 1
 
 
@@ -13,25 +13,33 @@ def get_number_of_pages():
     return page_data['number_of_pages']
 
 
+def get_date_from_timestamp(timestamp, timezone):
+    return datetime.fromtimestamp(timestamp, tz=timezone) if timestamp else None
+
+
 def load_attempts():
+
     pages = get_number_of_pages()
     for page in range(1, pages+1):
         page_data = requests.get(DEVMAN_API_URL, params={'page': page}).json()
         for user in page_data['records']:
-            yield [
-                user['username'],
-                user['timestamp'],
-                user['timezone']
-            ]
+            yield {
+                'username': user['username'],
+                'timestamp': user['timestamp'],
+                'timezone': user['timezone']
+            }
 
 
 def get_midnighters():
     midnighters_dict = {}
-    for username, timestamp, timezone_string in load_attempts():
-        timezone_tz = timezone(timezone_string)
+    for user in load_attempts():
+        username = user['username']
+        timestamp = user['timestamp']
+        tz = timezone(user['timezone'])
+
         if timestamp:
-            time = datetime.fromtimestamp(timestamp, tz=timezone_tz)
-            if int(time.strftime('%H%M')) < INT_MORNING_TIME:
+            time = datetime.datetime.fromtimestamp(timestamp, tz=tz)
+            if time.time() < MORNING_TIME:
                 midnighters_dict[username] = time
     return midnighters_dict
 
@@ -40,5 +48,5 @@ if __name__ == '__main__':
     midnighters_dict = get_midnighters()
     print(u'Список полуночников:\n')
     for username, time in midnighters_dict.items():
-        print(u'Имя пользователя: {}\nМестное время отправки: {}\n'.format(
-            username, time.strftime("%y-%m-%d %H:%M:%S")))
+        print(u'Имя пользователя: {}\nМестное время отправки: {}\n'.format
+              (username, time.strftime("%y-%m-%d %H:%M:%S")))
